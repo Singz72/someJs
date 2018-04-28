324dd4243911bac69411a1939d8ec40def5d34f7
Zzx <599106418@qq.com>
2018-04-28 09:50:26 +0800

    一些改动

--- animate/rotation/rotation.js
+++ animate/rotation/rotation.js
@@ -1,14 +1,13 @@
+var D = {
+    rotation: 'rotation', //获取类名为rotation的id
+    aRotation: 'rotation_content', //获取类名为rotation_content的id
+    oNext: 'rotation_next', //获取类名为rotation_next的id
+    Oprev: 'rotation_prev', //获取类名为rotation_prev的id
+    siteTitle: 'site_category_bar_header_title', //获取类名为site_category_bar_header_title的id
+    title: []
+}
+var rotation = {
 
-    var D = {
-        rotation:'rotation',//获取类名为rotation的id
-        aRotation:'rotation_content',//获取类名为rotation_content的id
-        oNext:'rotation_next',//获取类名为rotation_next的id
-        Oprev:'rotation_prev',//获取类名为rotation_prev的id
-        siteTitle:'site_category_bar_header_title',//获取类名为site_category_bar_header_title的id
-        title:[]
-    }
-var rotation={
-		
     /*轮播结构
     <div class="rotation" id="rotation">
         <div id="rotation_content" class="rotation_content">
@@ -23,42 +22,43 @@ var rotation={
         <div class="rotation_prev rotation_icon" id="rotation_prev"></div>
         <div class="rotation_next rotation_icon" id="rotation_next"></div>
     </div>*/
-    
-    rotationFun:function(D){
-        var atitle = D.title,notitle,atitle;
+
+    rotationFun: function(D) {
+        var atitle = D.title,
+            notitle, atitle;
         //有小标题或者没有
-        if(!atitle){
+        if (!atitle) {
             notitle = false;
-        }else{
+        } else {
             notitle = true;
-            var acitytitle = $("#"+D.siteTitle);//小标题
+            var acitytitle = $("#" + D.siteTitle); //小标题
             acitytitle.html(atitle[0]);
         }
 
         //上移动画-->
-        var oBody = $("#"+D.rotation)[0];//鼠标移入移出对象
+        var oBody = $("#" + D.rotation)[0]; //鼠标移入移出对象
 
-        var aRotation = $("#"+D.aRotation);//滚条对象
-        var aBanner = aRotation.find('.rotation_banner');//内容 
-        var oNext = $("#"+D.oNext)[0];//下一页
-        var Oprev = $("#"+D.Oprev)[0];//上一页
+        var aRotation = $("#" + D.aRotation); //滚条对象
+        var aBanner = aRotation.find('.rotation_banner'); //内容 
+        var oNext = $("#" + D.oNext)[0]; //下一页
+        var Oprev = $("#" + D.Oprev)[0]; //上一页
 
         var aRotation_height = aRotation.height();
         var aBanneri = aBanner.find('.rotation_banner_i');
-        aBanneri.height(aRotation_height)//内容框
+        aBanneri.height(aRotation_height) //内容框
 
-        var num = 0;//锁定当前图片
+        var num = 0; //锁定当前图片
         var l = aBanner.length;
         //隐藏所有div
-        for(var i = 0;i<l;i++){
-            aBanner[i].style.display='none';
+        for (var i = 0; i < l; i++) {
+            aBanner[i].style.display = 'none';
         }
         aBanner[0].style.display = 'block';
         aBanner[0].style.opacity = "1";
         aBanner[0].classList.add('rotation_on')
 
-        var timerTop;//上移计时器
-        var timerLeftright;//下一页计时器
+        var timerTop; //上移计时器
+        var timerLeftright; //下一页计时器
 
         //动画启动
         function startAnimate() {
@@ -94,7 +94,7 @@ var rotation={
                     aRotation[0].scrollTop = 0;
                     aBanner[i].classList.remove("rotation_on");
                     aBanner[i].style.opacity = "0";
-                    aBanner[i].style.display='none';
+                    aBanner[i].style.display = 'none';
                     i++;
                     num++;
                     if (num > l - 1) {
@@ -103,10 +103,10 @@ var rotation={
                     if (i > l - 1) {
                         i = 0;
                     }
-                    if(notitle){
+                    if (notitle) {
                         acitytitle.html(atitle[num]);
                     }
-                    aBanner[i].style.display='block';
+                    aBanner[i].style.display = 'block';
                     aBanner[i].classList.add("rotation_on");
                     aBanner[i].style.opacity = "1";
                 }
@@ -124,7 +124,7 @@ var rotation={
                     aRotation[0].scrollTop = 0;
                     aBanner[i].classList.remove("rotation_on");
                     aBanner[i].style.opacity = "0";
-                    aBanner[i].style.display='none';
+                    aBanner[i].style.display = 'none';
                     i--;
                     num--;
                     if (num < 0) {
@@ -133,11 +133,11 @@ var rotation={
                     if (i < 0) {
                         i = l - 1;
                     }
-                    if(notitle){
+                    if (notitle) {
                         acitytitle.html(atitle[num]);
                     }
 
-                    aBanner[i].style.display='block';
+                    aBanner[i].style.display = 'block';
                     aBanner[i].classList.add("rotation_on");
                     aBanner[i].style.opacity = "1";
                 }
@@ -153,9 +153,9 @@ var rotation={
                 console.log(aBanner[i])
                 aBanner[i].classList.remove("rotation_on");
                 aBanner[i].style.opacity = "0";
-                aBanner[i].style.display='none';
+                aBanner[i].style.display = 'none';
             }
-            aBanner[num].style.display='block';
+            aBanner[num].style.display = 'block';
             aBanner[num].classList.add("rotation_on");
             aBanner[num].style.opacity = "1";
 
@@ -168,13 +168,13 @@ var rotation={
         function TimeToNext() { /*设置定时器运行的函数*/
             num++;
             if (num < l) {
-                if(notitle){
+                if (notitle) {
                     acitytitle.html(atitle[num]);
                 }
                 showhide(num);
             } else {
                 num = 0;
-                if(notitle){
+                if (notitle) {
                     acitytitle.html(atitle[num]);
                 }
                 showhide(num);
--- tool/toolFuns.js
+++ tool/toolFuns.js
@@ -38,3 +38,32 @@ String.prototype.colorRgba = function(g) {
 var sHex = "#00538b";
 var sRgbColor = sHex.colorRgba(); //转为RGBA颜色值的方法  
 console.log(sRgbColor); //rgba(0,83,139,1)
+
+
+const _zx = {
+    /**
+     * 保留小数函数
+     * num:待转换的数
+     * x:保留的位数
+     * floor:是否直接截取不四舍五入
+     */
+    toDecimal: function(num, x, floor) {
+        if (isNaN(num)) {
+            console.log("请传入数字！！")
+            return
+        }
+        if (parseInt(num) == num) {
+            var s = num.toString() + '.';
+            var i = 0;
+            for (; i < x; i++) {
+                s = s + "0";
+            }
+            return s
+        } else {
+            if (floor == true) {
+                return Math.floor(num * Math.pow(10, x)) / Math.pow(10, x)
+            }
+            return Math.round(num * Math.pow(10, x)) / Math.pow(10, x);
+        }
+    }
+}
