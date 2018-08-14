b9c53511170590b3ca07ca260dcdb6640a7d448c
Zzx <599106418@qq.com>
2018-08-14 16:56:17 +0800

    +1

--- tool/toolFuns.js
+++ tool/toolFuns.js
@@ -524,3 +524,25 @@ var NumFuns = {
         return testStr.indexOf("," + val + ",") != -1;
     }
 }
+
+var arrayFun = {
+    //去重，也可以用ES6的set()去重
+    distinct: function(arr) {
+        var result = [];
+        arr.forEach(function(v, i, arr) { //这里利用map，filter方法也可以实现
+            var bool = arr.indexOf(v, i + 1); //从传入参数的下一个索引值开始寻找是否存在重复
+            if (bool === -1) {
+                result.push(v);
+            }
+        })
+        return result;
+    },
+    //数组中含对象元素  深拷贝
+    objDeepCopy: function(source) {
+        var sourceCopy = source instanceof Array ? [] : {};
+        for (var item in source) {
+            sourceCopy[item] = typeof source[item] === 'object' ? objDeepCopy(source[item]) : source[item];
+        }
+        return sourceCopy;
+    }
+}
