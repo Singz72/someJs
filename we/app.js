956f292f3b6879c6262d4ea615def604fc2a3558
zissou72 <599106418@qq.com>
2018-09-04 23:38:29 +0800

    添加一个小程序代码

---
+++ we/app.js
@@ -0,0 +1,9 @@
+//app.js
+App({
+    onLaunch: function() {
+        console.log('启动完小程序');
+    },
+    globalData: {
+        userInfo: null
+    }
+})
---
+++ we/pages/week/week.js
@@ -0,0 +1,14 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+    data: {
+        name: ''
+    },
+    onLoad: function() {
+        this.setData({
+            name: 'week'
+        })
+    }
+})
