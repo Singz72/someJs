90b19170bffa1dd3ede53e86f2a6b0c0ea94153d
Singz72 <599106418@qq.com>
2018-10-16 17:23:37 +0800

    +1

--- we/pages/account/index.js
+++ we/pages/account/index.js
@@ -2,136 +2,149 @@
 //获取应用实例
 import * as echarts from '../../ec-canvas/echarts';
 
-// const app = getApp();
+const app = getApp();
 
-function initCharts(canvas, width, height) {
-    const chart = echarts.init(canvas, null, {
-        width: width,
-        height: height
-    });
-    canvas.setChart(chart);
 
-    var option = {
-        title: {
-            text: '',
-            left: 'center'
-        },
-        color: ["#ffcc33"],
-        legend: {
-            data: [''],
-            top: 50,
-            left: 'center',
-            z: 100
-        },
-        grid: {
-            containLabel: true
-        },
-        tooltip: {
-            show: true,
-            trigger: 'axis'
-        },
-        axisPointer: {
-            type: 'line',
-            lineStyle: {
-                type: 'dashed'
-            }
-        },
-        xAxis: {
-            name: '',
-            type: 'value',
-            // boundaryGap: true,
-            data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-            // show: false,
-            axisLine: {
-                show: false,
-                lineStyle: {
-                    color: '#7f8389'
-                }
-            },
-            splitLine: {
-                lineStyle: {
-                    type: 'dashed',
-                    color: ['rgba(0,0,0,0)', '#ddd']
-                }
-            },
-            interval: 3,
-            scale: true,
-            axisTick: {
-                show: false
-            }
-        },
-        yAxis: {
-            name: '',
-            x: 'center',
-            type: 'value',
-            splitLine: {
-                lineStyle: {
-                    type: 'dashed',
-                    color: ['rgba(0,0,0,0)', '#ddd']
-                }
-            },
-            // show: false
-            axisLine: {
-                show: false,
-                lineStyle: {
-                    color: '#7f8389'
-                }
-            },
-            axisTick: {
-                show: false
-            }
-        },
-        series: [{
-            name: '消费',
-            type: 'line',
-            smooth: true,
-            data: [
-                [1, 18],
-                [2, 36],
-                [3, 65],
-                [4, 30],
-                [5, 78],
-                [6, 40],
-                [7, 33],
-                [8, 12],
-                [9, 50],
-                [10, 51],
-                [11, 35],
-                [12, 70],
-                [13, 35],
-                [14, 70],
-                [15, 35],
-                [16, 70],
-                [17, 35],
-                [18, 70],
-                [19, 35],
-                [20, 70],
-                [21, 18],
-                [22, 36],
-                [23, 65],
-                [24, 30],
-                [25, 78],
-                [26, 40],
-                [27, 33],
-                [28, 12],
-                [29, 50],
-                [30, 51],
-            ]
-        }]
-    };
-
-    chart.setOption(option);
-    return chart;
-}
+var dataList = [
+    [1, 18],
+    [2, 36],
+    [3, 65],
+    [4, 30],
+    [5, 78],
+    [6, 40],
+    [7, 33],
+    [8, 12],
+    [9, 50],
+    [10, 51],
+    [11, 35],
+    [12, 70]
+];
+var Chart = null;
 
 Page({
     data: {
         count: '100.00',
         ec: {
-            onInit: initCharts
-        }
+            lazyLoad: true
+        },
+        aFood: 'a_food',
+        aCar: 'a_car',
+        aShopping: 'a_shopping',
+        aMoney: 'a_money',
+    },
+    onLoad: function() {
+        this.echartsComponment = this.selectComponent('#mychart-dom-line');
+        this.getData();
     },
     onReady: function() {
         this.selectComponent('#timeHaveIcon').loadChangeDate();
-    }
+        setInterval(() => {
+            this.set_option(Chart);
+        }, 4000)
+    },
+    getData: function() {
+        if (!Chart) {
+            this.init_charts();
+        } else {
+            this.set_option(Chart);
+        }
+    },
+    init_charts: function() {
+        this.echartsComponment.init((canvas, width, height) => {
+            Chart = echarts.init(canvas, null, {
+                width: width,
+                height: height
+            });
+
+            this.set_option(Chart);
+            return Chart;
+        })
+    },
+    set_option: function(Chart) {
+        // Chart.clear();
+        Chart.setOption(this.get_option());
+    },
+    get_option: function() {
+        var option = {
+            title: {
+                text: '',
+                left: 'center'
+            },
+            color: ["#ffcc33"],
+            legend: {
+                data: [''],
+                top: 50,
+                left: 'center',
+                z: 100
+            },
+            grid: {
+                containLabel: true
+            },
+            tooltip: {
+                show: true,
+                trigger: 'axis',
+                renderMode: 'richText',
+                confine: true
+            },
+            axisPointer: {
+                type: 'line',
+                lineStyle: {
+                    type: 'dashed'
+                }
+            },
+            xAxis: {
+                name: '',
+                type: 'value',
+                // boundaryGap: true,
+                data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                // show: false,
+                axisLine: {
+                    show: false,
+                    lineStyle: {
+                        color: '#7f8389'
+                    }
+                },
+                splitLine: {
+                    lineStyle: {
+                        type: 'dashed',
+                        color: ['rgba(0,0,0,0)', '#ddd']
+                    }
+                },
+                interval: 3,
+                scale: true,
+                axisTick: {
+                    show: false
+                }
+            },
+            yAxis: {
+                name: '',
+                x: 'center',
+                type: 'value',
+                splitLine: {
+                    lineStyle: {
+                        type: 'dashed',
+                        color: ['rgba(0,0,0,0)', '#ddd']
+                    }
+                },
+                // show: false
+                axisLine: {
+                    show: false,
+                    lineStyle: {
+                        color: '#7f8389'
+                    }
+                },
+                axisTick: {
+                    show: false
+                }
+            },
+            series: [{
+                name: '消费',
+                type: 'line',
+                smooth: true,
+                data: dataList
+            }]
+        };
+        return option
+    },
+    categoryChangeIcon: function() {}
 })
